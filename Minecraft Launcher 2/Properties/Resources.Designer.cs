9308b46e2ce99a7781f236392cef90642f1ded93
psvm <psvmemail>
2019-02-07 00:54:00 +0900

    warning added

--- Minecraft Launcher 2/Properties/Resources.Designer.cs
+++ Minecraft Launcher 2/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Minecraft_Launcher_2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap warning {
+            get {
+                object obj = ResourceManager.GetObject("warning", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
